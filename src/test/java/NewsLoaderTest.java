7d03c6e473077dbef4158341cd07ea818f3d4287
adamsylla97 <209474@edu.p.lodz.pl>
2019-04-22 21:10:08 +0200

    *code formatting *removed unused imports

--- src/test/java/NewsLoaderTest.java
+++ src/test/java/NewsLoaderTest.java
@@ -8,39 +8,34 @@ import org.mockito.Mockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.mock;
 import static org.powermock.api.mockito.PowerMockito.*;
+
 import org.powermock.modules.junit4.PowerMockRunner;
-import static org.junit.Assert.*;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.core.classloader.annotations.PrepareForTest;
+
 import org.powermock.reflect.Whitebox;
 
 import java.util.List;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({ConfigurationLoader.class, NewsReaderFactory.class})
-public class NewsLoaderTest {
+@RunWith(PowerMockRunner.class) @PrepareForTest({ConfigurationLoader.class, NewsReaderFactory.class}) public class NewsLoaderTest {
 
     private NewsLoader newsLoader;
-    private NewsReaderFactory newsReaderFactory;
     private ConfigurationLoader configurationLoader;
     private PublishableNews publishableNews;
+    private NewsReaderFactory newsReaderFactory;
 
     @Before
-    public void setup(){
+    public void setup() {
         newsLoader = new NewsLoader();
         Configuration configuration = new Configuration();
         IncomingNews incomingNews = new IncomingNews();
 
-        incomingNews.add(new IncomingInfo("first",SubsciptionType.A));
-        incomingNews.add(new IncomingInfo("second",SubsciptionType.B));
-        incomingNews.add(new IncomingInfo("third",SubsciptionType.C));
-        incomingNews.add(new IncomingInfo("none",SubsciptionType.NONE));
+        incomingNews.add(new IncomingInfo("first", SubsciptionType.A));
+        incomingNews.add(new IncomingInfo("second", SubsciptionType.B));
+        incomingNews.add(new IncomingInfo("third", SubsciptionType.C));
+        incomingNews.add(new IncomingInfo("none", SubsciptionType.NONE));
 
         mockStatic(ConfigurationLoader.class);
         configurationLoader = mock(ConfigurationLoader.class);
@@ -56,25 +51,24 @@ public class NewsLoaderTest {
     }
 
     @Test
-    public void loadConfigurationMethodShouldBeCalledOnce(){
+    public void loadConfigurationMethodShouldBeCalledOnce() {
         publishableNews = newsLoader.loadNews();
         verify(configurationLoader, Mockito.times(1)).loadConfiguration();
     }
 
     @Test
-    public void incommingNewsShouldBeCorrectlyDevided(){
+    public void incommingNewsShouldBeCorrectlyDevided() {
         publishableNews = newsLoader.loadNews();
-        List<String> publicMessages = Whitebox.getInternalState(publishableNews,"publicContent");
+        List<String> publicMessages = Whitebox.getInternalState(publishableNews, "publicContent");
         List<String> subscribeMessages = Whitebox.getInternalState(publishableNews, "subscribentContent");
 
-        Assert.assertEquals(1,publicMessages.size());
-        Assert.assertEquals(3,subscribeMessages.size());
+        Assert.assertEquals(1, publicMessages.size());
+        Assert.assertEquals(3, subscribeMessages.size());
 
-        Assert.assertEquals("none",publicMessages.get(0));
-        Assert.assertEquals("first",subscribeMessages.get(0));
-        Assert.assertEquals("second",subscribeMessages.get(1));
-        Assert.assertEquals("third",subscribeMessages.get(2));
+        Assert.assertEquals("none", publicMessages.get(0));
+        Assert.assertEquals("first", subscribeMessages.get(0));
+        Assert.assertEquals("second", subscribeMessages.get(1));
+        Assert.assertEquals("third", subscribeMessages.get(2));
     }
-
 
 }
